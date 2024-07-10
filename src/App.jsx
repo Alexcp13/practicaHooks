c1632f989fbf200d7bb4cfd773e9f057d15b2b28
Alexcp13 <acp1320002@outlook.es>
2024-07-10 15:57:48 +0200

    s

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Character from './components/character/character';
+
+
+function App() {
+
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character").then((res) => res.json()).then((res) => setCharacters(res.results))
+  }, [])
+
+  return (
+    <>
+      {characters.map((character) => <Character character={character} />)}
+    </>
+  )
+}
+
+export default App;
